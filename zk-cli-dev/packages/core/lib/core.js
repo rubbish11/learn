348dfff286f8746d8f426cb96ae76cc053483e7f
hezk <hezk@citycloud.com.cn>
2024-05-09 16:45:10 +0800

    ss

--- zk-cli-dev/packages/core/lib/core.js
+++ zk-cli-dev/packages/core/lib/core.js
@@ -4,7 +4,7 @@ module.exports = core;
 const _ = require('@zk-cli/utils')
 
 console.log(_)
-console.log('hello world')
+console.log('hello world111')
 
 function core() {
   return 'Hello from core';
